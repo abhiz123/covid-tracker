9ed0df8ff86e9b05431f343d5571ef9920a71621
Abhiram Nair <abhirammnair@gmail.com>
2020-10-05 12:05:21 +0530

    Updates

---
+++ precache-manifest.31be269e8acca74a021d5aeb44f9a96b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3305a8706fe51f9722bd121c3e23b645",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "0ff07c9f71ee4721af76",
+    "url": "/covid-tracker/static/css/main.f092b67a.chunk.css"
+  },
+  {
+    "revision": "329e61dd9864fe1bba2b",
+    "url": "/covid-tracker/static/js/2.af5f5b52.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-tracker/static/js/2.af5f5b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff07c9f71ee4721af76",
+    "url": "/covid-tracker/static/js/main.1816f0b5.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
