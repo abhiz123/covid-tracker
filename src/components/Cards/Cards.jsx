031213e1002f5c75b4a511d0a4b70c18cd5b04a2
Abhiram Nair <abhirammnair@gmail.com>
2020-10-05 11:54:42 +0530

    Finished Graphs

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
 
-const Cards = () => {
-  return <h1>Cards</h1>;
+import styles from './Cards.module.css';
+
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infected"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of active cases from COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recovered"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of recoveries from COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Deaths"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of deaths caused by COVID-19."
+        />
+      </Grid>
+    </div>
+  );
 };
-
-export default Cards;
+export default Info;
